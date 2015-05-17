c4b6114b73eafd7225e0f3d43ca1431d1161c121
caio2k <caio2k@gmail.com>
2015-05-18 00:23:37 +0200

    changed vmlinux to zImage

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -17,14 +17,17 @@ git clone "git://github.com/${GITHUB_REPO_OWNER}/${GITHUB_REPO_NAME}.git"
 #compiling kernel
 cd "${GITHUB_REPO_NAME}"
 sb2 make "${KERNEL_CONFIG}"
+KERNEL_VERSION=`sb2 make kernelversion`
+export LOCALVERSION="-${GITHUB_REPO_OWNER}"
+KERNEL_NAME="${KERNEL_VERSION}-${GITHUB_REPO_OWNER}"
 sb2 make -j4 zImage
 sb2 make -j4 modules
 sb2 make modules_install INSTALL_MOD_PATH=./mods
-sb2 make install INSTALL_PATH=./mods
+mkdir ./mods/boot
+cp arch/arm/boot/zImage ./mods/boot/zImage_${KERNEL_NAME}
 
 #compressing kernel
-KERNEL_VERSION=`sb2 make kernelversion`
-FILE="n950-kernel-${KERNEL_VERSION}.tar.bz2"
+FILE="linux_${KERNEL_NAME}.tar.bz2"
 cd mods
 tar jcvf "/tmp/$FILE" *
 
