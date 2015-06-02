6a86e99f703d92c928b674ab41a54af4f3feeed8
caio2k <caio2k@gmail.com>
2015-06-02 22:32:38 +0200

    update travis init, removed mount using tmpfs

--- travis-linux-init.sh
+++ travis-linux-init.sh
@@ -54,7 +54,7 @@ mount -t tmpfs none /var/run
 cgroups-mount
 
 # mount /var/lib/docker with a tmpfs
-mount -t tmpfs none /var/lib/docker
+#mount -t tmpfs none /var/lib/docker
 
 # enable ipv4 forwarding for docker
 echo 1 > /proc/sys/net/ipv4/ip_forward
