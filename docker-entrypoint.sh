2a993ee2786960ab7efb8af353308c833df02ada
caio2k <caio2k@gmail.com>
2015-07-01 23:52:25 +0200

    updated entrypoint to use /input kernel or to download if nothing is present removed old travis-ci files

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,11 +11,12 @@ BINTRAY_REPO_OWNER=caio2k
 BINTRAY_REPO_NAME=$2
 BINTRAY_REPO_PACKAGE=$1
 
-#cloning kernel image
-#git clone "git://github.com/${GITHUB_REPO_OWNER}/${GITHUB_REPO_NAME}.git"
+#cloning kernel image if /input is empty 
+if [ ! "$(ls -A /input)" ]; then
+  git clone "git://github.com/${GITHUB_REPO_OWNER}/${GITHUB_REPO_NAME}.git" /input
+fi
 
 #compiling kernel
-#cd "${GITHUB_REPO_NAME}"
 cd /input/
 sb2 make "${KERNEL_CONFIG}"
 KERNEL_VERSION=`sb2 make kernelversion`
@@ -31,34 +32,4 @@ cp arch/arm/boot/zImage ./mods/boot/zImage_${KERNEL_NAME}
 FILE="linux_${KERNEL_NAME}.tar.bz2"
 cd mods
 tar jcvf "/output/$FILE" *
-
-if [ -z "$BINTRAY_APIKEY" ]; then
-  echo "build done but it will not be uploaded to bintray due to missing APIKEY"
-  exit
-fi
-
-#copying it to binpaste
-BINTRAY_REPO_DESC="https://github.com/${GITHUB_REPO_OWNER}/${GITHUB_REPO_NAME}/README.md"
-BINTRAY_REPO_VERSION=${KERNEL_VERSION}
-
-HEADER1="X-Bintray-Package: $BINTRAY_REPO_PACKAGE"
-HEADER2="X-Bintray-Version: $BINTRAY_REPO_VERSION"
-HEADER3="X-Bintray-Publish: 1"
-HEADER4="X-Bintray-Override: 1"
-HEADER5="X-Bintray-Explode: 0"
-
-#check if repository exists
-[ `curl -sw "%{http_code}" -o /dev/null -X GET -u"$BINTRAY_USER:$BINTRAY_APIKEY" -H "$HEADER1" -H "$HEADER2" -H "$HEADER3" -H "$HEADER4" -H "$HEADER5" https://api.bintray.com/repos/$BINTRAY_REPO_OWNER/$BINTRAY_REPO_NAME` -eq 200 ] || exit 1
-
-#check if package already exists
-if [ `curl -sw "%{http_code}" -o /dev/null -u"$BINTRAY_USER:$BINTRAY_APIKEY" -X GET "https://api.bintray.com/packages/$BINTRAY_REPO_OWNER/$BINTRAY_REPO_NAME/$BINTRAY_REPO_PACKAGE"` -ne 200 ]; then
-  #if package doesn't exists, try to create it
-  echo "Creating package $BINTRAY_REPO_PACKAGE"
-  JSON_CREATE_PACKAGE="{ \"name\": \"$BINTRAY_REPO_PACKAGE\", \"desc\": \"auto\", \"desc_url\": \"$BINTRAY_REPO_DESC\", \"labels\": \"\", \"licenses\": [\"GPL-2.0\"], \"vcs_url\": \"kernel.org\" }"
-  curl -sw "%{http_code}" -o /dev/null -u"$BINTRAY_USER:$BINTRAY_APIKEY" -H "Content-Type: application/json" -X POST "https://api.bintray.com/packages/$BINTRAY_REPO_OWNER/$BINTRAY_REPO_NAME" --data "$JSON_CREATE_PACKAGE"
-fi
-
-#upload file
-cd /tmp
-curl -vvf -T $FILE -u"$BINTRAY_USER:$BINTRAY_APIKEY" -H "$HEADER1" -H "$HEADER2" -H "$HEADER3" -H "$HEADER4" -H "$HEADER5" https://api.bintray.com/content/$BINTRAY_REPO_OWNER/$BINTRAY_REPO_NAME/kernel/$BINTRAY_REPO_PACKAGE/
 
