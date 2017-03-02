b142eb328bc8eafaaf14ce998cd3efd90739f559
caio2k <caio2k@gmail.com>
2017-03-02 01:27:32 +0100

    added script prepare_modules.sh from mer

--- Dockerfile
+++ Dockerfile
@@ -5,8 +5,9 @@ MAINTAINER caio2k
 WORKDIR /srv/mer/targets/n950rootfs/root
 RUN /root/./sb2_wrapper.exp 'zypper in perl ncurses-devel'
 
-COPY docker-entrypoint.sh /srv/mer/targets/n950rootfs/root/
 VOLUME ["/input","/output"]
-RUN  chmod +x /srv/mer/targets/n950rootfs/root/docker-entrypoint.sh
+COPY docker-entrypoint.sh /srv/mer/targets/n950rootfs/root/
+COPY prepare_modules.sh /srv/mer/targets/n950rootfs/root/
+RUN  chmod +x /srv/mer/targets/n950rootfs/root/*.sh
 ENTRYPOINT ["/srv/mer/targets/n950rootfs/root/docker-entrypoint.sh"]
 
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,35 +2,30 @@
 #by caio2k
 
 #Parameters
-GITHUB_REPO_OWNER=$1
-GITHUB_REPO_NAME=$2
-KERNEL_CONFIG=$3
+KERNEL_CONFIG=${1:-rm581_defconfig}
+GITHUB_REPO_OWNER=$2
+GITHUB_REPO_NAME=$3
 
+FOLDER=`dirname $0`
 HEADER="mer-n950-kernel DEBUG"
 
-echo "$HEADER getting UID/GID from /input folder"
 if [ -f /input/Makefile ]; then
-  TARGET_UID=$(stat -c "%u" /input/Makefile)
-  TARGET_GID=$(stat -c "%g" /input/Makefile)
-else
-  TARGET_UID=0
-  TARGET_GID=0
-fi
-
-if [ ! "$(ls -A /input/Makefile)" ]; then
-  echo "$HEADER cloning kernel image if /input is empty "
-  git clone git://github.com/${GITHUB_REPO_OWNER}/${GITHUB_REPO_NAME}.git /srv/mer/targets/n950rootfs/root/input
-else
   echo "$HEADER copying kernel source from /input"
   cp -r /input /srv/mer/targets/n950rootfs/root/input
+  export LOCALVERSION=""
+else
+  echo "$HEADER cloning kernel image from github because /input is empty"
+  git clone git://github.com/${GITHUB_REPO_OWNER}/${GITHUB_REPO_NAME}.git /srv/mer/targets/n950rootfs/root/input
+  export LOCALVERSION="-${GITHUB_REPO_OWNER}"
 fi
 
 echo "$HEADER selecting kernel_config ${KERNEL_CONFIG}" 
 cd /srv/mer/targets/n950rootfs/root/input
 sb2 make ${KERNEL_CONFIG} || exit 1
 KERNEL_VERSION=`sb2 make kernelversion`
-export LOCALVERSION="-${GITHUB_REPO_OWNER}"
-KERNEL_NAME="${KERNEL_VERSION}-${GITHUB_REPO_OWNER}"
+KERNEL_NAME="${KERNEL_VERSION}${LOCALVERSION}"
+echo "$HEADER kernel_version is ${KERNEL_VERSION}"
+echo "$HEADER kernel_name is ${KERNEL_NAME}" 
 
 echo "$HEADER compiling kernel zImage" 
 sb2 make -j4 zImage
@@ -41,16 +36,16 @@ sb2 make -j4 modules || exit 3
 echo "$HEADER installing modules in MOCK folder"
 sb2 make modules_install INSTALL_MOD_PATH=./mods || exit 4
 
-echo "$HEADER installing zImage in MOCK folder"
-mkdir ./mods/boot    || exit 5
-ls arch/arm/boot -alhtr
-cp arch/arm/boot/zImage ./mods/boot/zImage_${KERNEL_NAME} || exit 6
+echo "$HEADER launching prepare_modules.sh script"
+${FOLDER}/prepare_modules.sh "${KERNEL_VERSION}" "./mods" || exit 5
 
 echo "$HEADER compressing kernel in tarball"
 FILE="linux_${KERNEL_NAME}.tar.bz2"
 cd mods
 tar jcvf "/output/$FILE" *
 
-echo "$HEADER setting up tarball UID/GID as detected in /output"
+TARGET_UID=`stat -c '%u' /output`
+TARGET_GID=`stat -c '%g' /output`
+echo "$HEADER setting up tarball owner UID/GID ${TARGET_UID}/${TARGET_GID}"
 chown ${TARGET_UID}.${TARGET_GID} /output/$FILE
 
